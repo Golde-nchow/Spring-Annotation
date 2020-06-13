158a796aa1eccb638ec1a5c377de84345e1626c3
Golden_Chow <570384231@qq.com>
2020-06-13 10:37:39 +0800

    初始化项目

---
+++ src/main/java/cn/cjz/SpringAnnotionApplication.java
@@ -0,0 +1,13 @@
+package cn.cjz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAnnotionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAnnotionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/cjz/SpringAnnotionApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.cjz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAnnotionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
