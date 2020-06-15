f83f4f8c30b312ea5bc1aa2f5e206a4e386c6ff2
Golden_Chow <570384231@qq.com>
2020-06-15 23:32:29 +0800

    简单的用户控制器

---
+++ src/main/java/cn/cjz/web/UserController.java
@@ -0,0 +1,33 @@
+package cn.cjz.web;
+
+import cn.cjz.model.User;
+import cn.cjz.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: Kam-Chou
+ * @date: 2020/6/15 22:57
+ * @description: 注解控制器
+ * @version: 1.0
+ */
+@RestController
+@RequestMapping("user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 通过ID获取对应的用户信息
+     * @param id 用户的ID信息
+     * @return   用户信息字符串
+     */
+    @RequestMapping("{id}")
+    public User selectById(@PathVariable Integer id) {
+        return userService.findUserById(id);
+    }
+
+}
