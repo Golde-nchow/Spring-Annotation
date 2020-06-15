03e893c3a1146d48df600077650874d2ef57aa68
Golden_Chow <570384231@qq.com>
2020-06-16 00:22:17 +0800

    一定要把切面类注册到Spring容器中

--- src/main/java/cn/cjz/aspect/LogAspect.java
+++ src/main/java/cn/cjz/aspect/LogAspect.java
@@ -4,6 +4,7 @@ import cn.cjz.annotation.Log;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
 
 /**
  * @author: Kam-Chou
@@ -11,6 +12,7 @@ import org.aspectj.lang.annotation.Pointcut;
  * @description: 日志切面逻辑类
  * @version: 1.0
  */
+@Component
 @Aspect
 public class LogAspect {
 
